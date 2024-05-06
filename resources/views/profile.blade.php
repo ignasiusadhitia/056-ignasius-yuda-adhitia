43e4b74fb04cf19ddca4bce642a7c04d802ef98d
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-05-06 19:43:12 +0700

    add profile form to profile blade

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -6,5 +6,21 @@
 
     <h2>Profile</h2>
     <form action="{{ route('profile') }}" method="POST">
+        <div>
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" value="{{ auth()->user->name }}">
+            @error()
+                <div>{{ $message }}</div>
+            @enderror
+        </div>
 
+        <div>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="name" value="{{ auth()->user->email }}" disabled>
+        </div>
+
+        <div>
+            <button type="submit">Save Changes</button>
+        </div>
     </form>
+@endsection
