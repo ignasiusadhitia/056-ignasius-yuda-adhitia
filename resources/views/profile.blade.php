2963ccdfa672df79d29956db307956e510acd183
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-05-06 19:31:07 +0700

    create profile blade

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.layout')
+
+@section('title', 'Profile Page')
+
+@section('content')
+
+    <h2>Profile</h2>
+    <form action="{{ route('profile') }}" method="POST">
+
+    </form>
