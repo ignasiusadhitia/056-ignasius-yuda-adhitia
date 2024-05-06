c63852d7f5ad52c2735bc12a59cb0567b74c268d
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-05-06 18:15:25 +0700

    create register blade

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('title', 'Register Page')
+
+@section('content')
+
+    <form action="" method="POST">
+        <h2>Register</h2>
+
+        @csrf
+        <div>
+            <input type="text" name="name" id="name" placeholder="Name">
+            @error('name')
+                <div>{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div>
+            <input type="email" name="email" id="email" placeholder="Email">
+            @error('email')
+                <div>{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div>
+            <input type="password" name="password" id="password" placeholder="Password">
+            @error('password')
+                <div>{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div>
+            <button type="submit">Register</button>
+        </div>
+
+        <div>
+            <p>Already have an account? <a href="">Login</a></p>
+        </div>
+    </form>
+
+@endsection
