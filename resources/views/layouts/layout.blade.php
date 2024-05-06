b125eefc27e957753268204f610f0a18884c22f3
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-05-06 18:05:27 +0700

    add layout blade

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+</head>
+
+<body>
+
+</body>
+
+<header>
+    <nav>
+        <div>
+            <ul>
+                <li>
+                    <a href="">Dashboard</a>
+                    <a href="">Profile</a>
+                    <a href="">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+
+<div>
+    <div>
+        @session('success')
+            <div>
+                {{ session('success') }}
+            </div>
+        @endsession
+
+        @yield('content')
+    </div>
+</div>
+
+</html>
