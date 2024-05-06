43d5fde1cd610aef17f1c469bfee122e4eef3265
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-05-06 18:34:06 +0700

    create register route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['middleware' => ['guest']], function () {
+    Route::match(['get', 'post'], 'register', [AuthController::class, 'register'])->name('register');
 });
