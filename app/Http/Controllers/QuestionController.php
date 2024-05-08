f27ee0125f782d235af0ed18e384eaf933ff0a9a
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-05-08 15:26:58 +0700

    create update function on QuestionController

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -78,4 +78,54 @@ class QuestionController extends Controller
         $answers = $question->answers;
         return view('questions.edit', compact('question', 'categories', 'answers'));
     }
+
+    public function update(Request $request, Question $question)
+    {
+        if ($question->user_id !== Auth::id()) {
+            abort(403, 'Unauthorized action');
+        }
+
+        $validator = Validator::make($request->all(), [
+            'question_text' => 'required|string|max:255',
+            'difficulty_level' => 'required|in:easy,medium,hard',
+            'answer.*' => 'required|string|max:255',
+            'correct_answer' => 'required|integer|between:1,' . count($request->answer),
+
+            function ($attribute, $request) {
+                if (count($request->input($attribute)) < 2) {
+                    return false;
+                }
+                return true;
+            },
+        ]);
+
+        if ($validator->fails()) {
+            return back()->withErrors($validator)->withInput();
+        }
+
+        DB::transaction(function () use ($request, $question) {
+            $question->question_text = $request->question_text;
+            $question->difficulty_level = $request->difficulty_level;
+            $question->category_id = $request->category_id;
+            $question->image_url = $request->image_url;
+            $question->points = $question->calculatePoints($request->difficulty_level);
+
+            $question->save();
+
+            $question->answers()->delete();
+
+            $answers = [];
+            foreach ($request->answer as $key => $answerText) {
+                $answer = new Answer;
+                $answer->question_id = $question->id;
+                $answer->answer_text = $answerText;
+                $answer->is_correct = ($key + 1) == $request->correct_answer;
+                $answers[] = $answer;
+            }
+
+            $question->answers()->saveMany($answers);
+        });
+
+        return redirect()->route('questions.index')->with('success', 'Question updated successfully');
+    }
 }
