19325dc4aa9d99e848ce7daa9f0979728296af5b
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-05-08 14:23:59 +0700

    create table to display user created questions

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -1,1 +1,33 @@
 <h1>My Questions</h1>
+
+@if (session('success'))
+    <div>
+        {{ session('success') }}
+    </div>
+@endif
+
+@if ($questions->isEmpty())
+    <div>
+        You haven't created any questions yet.
+    </div>
+@else
+    <table>
+        <thead>
+            <tr>
+                <th>Question Text</th>
+                <th>Category</th>
+                <th>Difficulty</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($questions as $question)
+                <tr>
+                    <td>{{ $question->question_text }}</td>
+                    <td>{{ $question->category->name }}</td>
+                    <td>{{ $question->difficulty_level }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endif
