2fe08c6ecbf0c3ad74506b3db8eabf1a46691cb6
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-05-13 21:35:43 +0700

    create initial routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,39 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/questions', function () {
+    return view('questions.index');
+});
+
+Route::get('/questions/create', function () {
+    return view('questions.create');
+});
+
+Route::get('/questions/edit/{question}', function () {
+    return view('questions.edit');
+});
+
+Route::get('/trivia', function () {
+    return view('trivia');
+});
+
+Route::get('leaderboard', function () {
+    return view('leaderboard');
+});
