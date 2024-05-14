81884e6ad908a4fecb841593cbd3fcf99ae7afba
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-05-14 16:03:06 +0700

    add headline and description hero section of welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,22 @@
 @extends('partials.layouts.app')
 
-@section('title', 'Jabar Trivia - Home')
+@section('title', 'Home')
 
 @section('content')
 
-    <h1>Jabar Trivia</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, sapiente laudantium. Similique quis illo pariatur
-        ea laborum qui, maiores eius odio molestiae, esse ipsam blanditiis?</p>
+    <section class="section-wrapper">
+        <h1>Think You Know West Java? Unleash Your Inner Jabar Guru with Jabar Trivia!</h1>
+        <p>Calling all West Java enthusiasts! Dive into the world of Jabar Trivia, the ultimate test of your Jabar
+            knowledge.
+            Answer captivating trivia questions with beautiful images, conquer the leaderboards, and emerge as a certified
+            Jabar
+            Guru! Create your own trivia challenges, rack up points, and battle your friends for trivia supremacy. It's time
+            to
+            unleash your inner Jabar expert and embark on a fun-filled learning adventure!</p>
 
-    <a href="{{ route('register') }}">
-        <button>Register</button>
-    </a>
+        <a href="{{ route('register') }}">
+            <button>Register</button>
+        </a>
+    </section>
 
 @endsection
