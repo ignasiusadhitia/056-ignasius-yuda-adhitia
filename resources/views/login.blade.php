829c8b4571d0940bc4be20453ba91a66e51013dd
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-05-14 16:25:45 +0700

    create login initial page layout

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,3 +1,41 @@
-<div>
-    <h1>Login</h1>
-</div>
+@extends('partials.layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+
+    <section class="section-wrapper">
+        <h1>Login</h1>
+
+        <form action="" method="">
+            @csrf
+
+            <div>
+                <input type="email" name="email" id="email" placeholder="Email">
+                @error('email')
+                    <div>
+                        <span>{{ $message }}</span>
+                    </div>
+                @enderror
+            </div>
+
+            <div>
+                <input type="password" name="password" id="password" placeholder="Password">
+                @error('password')
+                    <div>
+                        <span>{{ $message }}</span>
+                    </div>
+                @enderror
+            </div>
+
+            <div>
+                <button type="submit">Login</button>
+            </div>
+
+            <div>
+                <p>Don't have an account? <a href="{{ route('register') }}">Register</a></p>
+            </div>
+        </form>
+    </section>
+
+@endsection
