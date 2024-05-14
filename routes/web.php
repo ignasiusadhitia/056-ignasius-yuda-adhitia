6d3d8d6f5449208a8f3bb120c6c55439e1e4e9b0
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-05-14 13:34:11 +0700

    add login and register route naming

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,11 @@ Route::get('/', function () {
 
 Route::get('/register', function () {
     return view('register');
-});
+})->name('register');
 
 Route::get('/login', function () {
     return view('login');
-});
+})->name('login');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
