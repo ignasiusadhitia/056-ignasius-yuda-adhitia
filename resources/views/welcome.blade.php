4c4cca9e9b869cccd40060819869461497c2a005
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-05-14 13:27:15 +0700

    create initial style.css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,49 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('partials.layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('title', 'Jabar Trivia - Home')
 
-    <title>Jabar Trivia</title>
+@section('content')
 
-    <!-- Fonts -->
+    <h1>Jabar Trivia</h1>
 
-    <!-- Styles -->
-
-</head>
-
-<body>
-    <header>
-        <span>Jabar Trivia</span>
-
-        @if (Route::has('login'))
-            <nav>
-                @auth
-                    <a href="{{ url('/dashboard') }}">
-                        Dashboard
-                    </a>
-                @else
-                    <a href="{{ route('login') }}">
-                        Log in
-                    </a>
-
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}">
-                            Register
-                        </a>
-                    @endif
-                @endauth
-            </nav>
-        @endif
-    </header>
-
-    <main>
-    </main>
-
-    <footer>
-        <span>Jabar Trivia &copy; {{ date('Y') }}</span>
-    </footer>
-</body>
-
-</html>
+@endsection
