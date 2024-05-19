e137d9f0328f1d33a79f91f14d35aa5294d6dd98
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-05-19 20:11:29 +0700

    fix dashboard page layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-@extends('layouts.layout')
-
-@section('title', 'Dashboard Page')
-
-@section('content')
-
-    <h2>Hi {{ auth()->user()->name }}, Welcome to Jabar Trivia</h2>
-
-    @if (auth()->user()->image)
-        <img src="{{ asset('images/' . auth()->user()->image) }}" alt="profile" />
-    @endif
-
-    <a href="{{ route('trivia.question') }}">Play</a>
-=======
 @extends('partials.layouts.app')
 
 @section('title', 'Dashboard')
@@ -25,10 +10,12 @@
         <div class="user-wrapper">
             <div class="greeting-wrapper">
                 <div>
-                    <h2>Hi, User 1</h2>
+                    <h2>Hi, {{ auth()->user()->name }}</h2>
                     <p>Let's make this day productive</p>
                 </div>
-                <img src="{{ asset('assets/images/avatar-1.jpg') }}" alt="user-1">
+                @if (auth()->user()->image)
+                    <img src="{{ asset('images/' . auth()->user()->image) }}" alt="profile-photo">
+                @endif
             </div>
 
             <div class="rank-wrapper">
@@ -72,6 +59,5 @@
         </div>
 
     </section>
->>>>>>> 86f418071df7aeb26dc3ee0dddde2e6864224544
 
 @endsection
