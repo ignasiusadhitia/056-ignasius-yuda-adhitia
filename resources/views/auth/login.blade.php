51015ca7974f20c145f4125007b6a963c1d8e6af
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-05-19 20:06:41 +0700

    fix login page layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,32 +1,43 @@
-@extends('layouts.app')
+@extends('partials.layouts.app')
 
-@section('title', 'Login Page')
+@section('title', 'Login')
 
 @section('content')
 
-    <form action="{{ route('login') }}" method="POST">
-        <h2>Login</h2>
+    <section class="section-wrapper">
+        <h1>Login</h1>
 
-        @csrf
-        <div>
-            <input type="email" name="email" id="email" placeholder="Email">
-            @error('email')
-                <div>{{ $message }}</div>
-            @enderror
-        </div>
+        <form action="{{ route('login') }}" method="POST">
+            @csrf
 
-        <div>
-            <input type="password" name="password" id="password" placeholder="Password">
-            @error('password')
-                <div>{{ $message }}</div>
-            @enderror
-        </div>
+            <div class="field-wrapper">
+                <label for="email">Email:</label>
+                <input type="email" name="email" id="email">
+                @error('email')
+                    <div>
+                        <span>{{ $message }}</span>
+                    </div>
+                @enderror
+            </div>
 
-        <div>
-            <button type="submit">Login</button>
-        </div>
+            <div class="field-wrapper">
+                <label for="password">Password:</label>
+                <input type="password" name="password" id="password">
+                @error('password')
+                    <div>
+                        <span>{{ $message }}</span>
+                    </div>
+                @enderror
+            </div>
 
-        <div>
-            <p>Don't have an account? <a href="{{ route('register') }}">Register</a></p>
-        </div>
-    </form>
+            <div>
+                <button type="submit">Login</button>
+            </div>
+
+            <div>
+                <p>Don't have an account? <a href="{{ route('register') }}">Register</a></p>
+            </div>
+        </form>
+    </section>
+
+@endsection
