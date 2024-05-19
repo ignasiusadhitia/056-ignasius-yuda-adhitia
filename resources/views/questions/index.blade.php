00d0f981ac338e8f24df8ab9cc77b8292f687260
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-05-19 20:56:26 +0700

    fix questions page layout

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-<h1>My Questions</h1>
-
-@if (session('success'))
-    <div>
-        {{ session('success') }}
-    </div>
-@endif
-
-@if ($questions->isEmpty())
-    <div>
-        You haven't created any questions yet.
-    </div>
-@else
-    <table>
-        <thead>
-            <tr>
-                <th>Question Text</th>
-                <th>Category</th>
-                <th>Difficulty</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($questions as $question)
-                <tr>
-                    <td>{{ $question->question_text }}</td>
-                    <td>{{ $question->category->name }}</td>
-                    <td>{{ $question->difficulty_level }}</td>
-                    <td>
-                        <a href="{{ route('questions.edit', $question) }}">Edit</a>
-                        <form action="{{ route('questions.destroy', $question) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-
-                            <button type="submit"
-                                onclick="return confirm('Are you want to delete this question?')">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-@endif
-=======
 @extends('partials.layouts.app')
 
 @section('title', 'My Question')
@@ -51,32 +6,32 @@
 
     <section class="section-wrapper">
         <h1>My Questions</h1>
+        @if ($questions->isEmpty())
+            <p>
+                You haven't created any questions yet.
+            </p>
+        @else
+            <div class="questions-wrapper">
+                @foreach ($questions as $key => $question)
+                    <div class="question-item">
+                        <div>{{ $key + 1 }}</div>
+                        <div>{{ $question->question_text }}</div>
+                        <div>{{ $question->category->name }}</div>
+                        <div>{{ $question->difficulty_level }}</div>
+                        <div>
+                            <a href="{{ route('questions.edit', $question) }}">Edit</a>
+                            <form action="{{ route('questions.destroy', $question) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
 
-        <div class="questions-wrapper">
-            <div class="question-item">
-                <div>1.</div>
-                <div>Lorem ipsum dolor sit.</div>
-                <div>Geography</div>
-                <div>Easy</div>
-                <div>
-                    <a href="">Edit</a>
-                    <a href="">Delete</a>
-                </div>
+                                <button type="submit"
+                                    onclick="return confirm('Are you want to delete this question?')">Delete</button>
+                            </form>
+                        </div>
+                    </div>
+                @endforeach
             </div>
-
-            <div class="question-item">
-                <div>2.</div>
-                <div>Lorem ipsum dolor sit.</div>
-                <div>Geography</div>
-                <div>Easy</div>
-                <div>
-                    <a href="">Edit</a>
-                    <a href="">Delete</a>
-                </div>
-            </div>
-            </tbody>
-        </div>
     </section>
 
+    @endif
 @endsection
->>>>>>> 86f418071df7aeb26dc3ee0dddde2e6864224544
