a6a9fe68fa41cdddbd5d776fa304f556ebeb5862
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-05-19 20:01:57 +0700

    fix register page layout

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,41 +1,54 @@
-@extends('layouts.app')
+@extends('partials.layouts.app')
 
-@section('title', 'Register Page')
+@section('title', 'Register')
 
 @section('content')
 
-    <form action="" method="POST">
-        <h2>Register</h2>
+    <section class="section-wrapper">
 
-        @csrf
-        <div>
-            <input type="text" name="name" id="name" placeholder="Name">
-            @error('name')
-                <div>{{ $message }}</div>
-            @enderror
-        </div>
+        <h1>Register</h1>
 
-        <div>
-            <input type="email" name="email" id="email" placeholder="Email">
-            @error('email')
-                <div>{{ $message }}</div>
-            @enderror
-        </div>
+        <form action="" method="POST">
+            @csrf
 
-        <div>
-            <input type="password" name="password" id="password" placeholder="Password">
-            @error('password')
-                <div>{{ $message }}</div>
-            @enderror
-        </div>
+            <div class="field-wrapper">
+                <label for="name">Name:</label>
+                <input type="text" name="name" id="name">
+                @error('name')
+                    <div>
+                        <span>{{ $message }}</span>
+                    </div>
+                @enderror
+            </div>
 
-        <div>
-            <button type="submit">Register</button>
-        </div>
+            <div class="field-wrapper">
+                <label for="email">Email:</label>
+                <input type="email" name="email" id="email">
+                @error('email')
+                    <div>
+                        <span>{{ $message }}</span>
+                    </div>
+                @enderror
+            </div>
 
-        <div>
-            <p>Already have an account? <a href="{{ route('login') }}">Login</a></p>
-        </div>
-    </form>
+            <div class="field-wrapper">
+                <label for="password">Password:</label>
+                <input type="password" name="password" id="password">
+                @error('password')
+                    <div>
+                        <span>{{ $message }}</span>
+                    </div>
+                @enderror
+            </div>
+
+            <div>
+                <button type="submit">Register</button>
+            </div>
+
+            <div>
+                <p>Already have an account? <a href="{{ route('login') }}">Login</a></p>
+            </div>
+        </form>
+    </section>
 
 @endsection
