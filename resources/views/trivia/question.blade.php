56579bec3312a7adff2236fabfd548dfc77b72a4
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-05-20 21:04:48 +0700

    change radio input to button

--- resources/views/trivia/question.blade.php
+++ resources/views/trivia/question.blade.php
@@ -1,24 +1,45 @@
-@extends('layouts.app')
+@extends('partials.layouts.app')
+
+@section('title', 'Trivia')
 
 @section('content')
-    <h1>Trivia Play</h1>
+    <section class="section-wrapper">
+        <h1>Trivia Play</h1>
 
-    <div class="container">
-        @if (session('status'))
-            <div class="alert alert-success">{{ session('status') }}</div>
-        @endif
+        <div class="trivia-wrapper">
+            @if (session('status'))
+                <div class="alert alert-success">{{ session('status') }}</div>
+            @endif
 
-        <h2>{{ $question->question_text }}</h2>
-        <form action="{{ route('trivia.answer') }}" method="POST">
-            @csrf
-            <input type="hidden" name="question_id" value="{{ $question->id }}">
-            @foreach ($question->answers as $answer)
-                <div>
-                    <input type="radio" name="answer_id" value="{{ $answer->id }}" required>
-                    <label>{{ $answer->answer_text }}</label>
+            <div class="trivia-wrapper">
+                <div class="question-wrapper">
+                    @if ($question->image_url)
+                        <img src="{{ $question->image_url }}" alt="question-image">
+                    @endif
+                    <p>{{ $question->question_text }}</p>
                 </div>
-            @endforeach
-            <button type="submit" class="btn btn-primary">Submit Answer</button>
-        </form>
-    </div>
+            </div>
+            <form id="triviaForm" action="{{ route('trivia.answer') }}" method="POST" class="answers-wrapper">
+                @csrf
+                <input type="hidden" name="question_id" value="{{ $question->id }}">
+                <input type="hidden" id="answer_id" name="answer_id" value="">
+
+                @foreach ($question->answers as $answer)
+                    <div class="answer-item">
+                        <button type="button" class="btn-answer"
+                            data-answer-id="{{ $answer->id }}">{{ $answer->answer_text }}</button>
+                    </div>
+                @endforeach
+            </form>
+        </div>
+    </section>
+
+    <script>
+        document.querySelectorAll('.btn-answer').forEach(button => {
+            button.addEventListener('click', function() {
+                document.getElementById('answer_id').value = this.getAttribute('data-answer-id');
+                document.getElementById('triviaForm').submit()
+            })
+        })
+    </script>
 @endsection
