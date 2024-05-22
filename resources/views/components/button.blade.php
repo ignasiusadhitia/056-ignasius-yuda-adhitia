ef7dbe6d0f6e5d15360f278ab6f3360bf1048552
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-05-22 12:10:50 +0700

    fix button component so it can receive icon and text

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,12 @@
-<div>
-    <!-- Order your soul. Reduce your wants. - Augustine -->
-</div>
+@props(['type' => 'submit', 'icon' => null, 'text' => null])
+
+<button type="{{ $type }}">
+    @if ($icon)
+        <i class="{{ $icon }}"></i>
+    @endif
+    @if ($text)
+        <span>{{ $text }}</span>
+    @elseif ($slot->isNotEmpty())
+        {{ $slot }}
+    @endif
+</button>
