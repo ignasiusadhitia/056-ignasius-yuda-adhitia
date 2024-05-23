5f34ac4df4099d1b8fd6b2daf1641d2b8de1a003
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-05-23 20:39:14 +0700

    fix bottom-navigation style

--- resources/views/components/bottom-navigation.blade.php
+++ resources/views/components/bottom-navigation.blade.php
@@ -1,21 +1,21 @@
 <nav class="bottom-navigation section-wrapper">
     <ul>
         <li>
-            <a href="{{ route('dashboard') }}">
-                <i class="fas fa-home"></i>
-                <span>Home</span>
-            </a>
-        </li>
-        <li>
             <a href="{{ route('questions.index') }}">
                 <i class="fas fa-question"></i>
-                <span>My Questions</span>
+                <span>Questions</span>
             </a>
         </li>
         <li>
             <a href="{{ route('trivia.index') }}">
                 <i class="fas fa-play"></i>
                 <span>Play</span>
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('leaderboard') }}">
+                <i class="fa-solid fa-award"></i>
+                <span>Leaderboard</span>
             </a>
         </li>
         <li>
@@ -33,41 +33,3 @@
         </li>
     </ul>
 </nav>
-
-<style>
-    .bottom-navigation {
-        position: fixed;
-        max-width: 42em;
-        bottom: 0;
-        box-shadow: 0 -1px 6px rgba(0, 0, 0, 0.1);
-        z-index: 1000;
-    }
-
-    .bottom-navigation ul {
-        display: flex;
-        justify-content: space-between;
-        margin: 0;
-        padding: 0;
-        list-style: none;
-    }
-
-    .bottom-navigation li {
-        flex: 1;
-        text-align: center;
-    }
-
-    .bottom-navigation a {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        padding: 10px;
-        color: #333;
-        text-decoration: none;
-        font-size: 12px;
-    }
-
-    .bottom-navigation a i {
-        font-size: 20px;
-    }
-</style>
