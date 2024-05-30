e2f6e4cbeac34416efe8dab53f9828c476221868
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-05-30 12:01:07 +0700

    relocate alert script

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,4 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function (event) {
+    // ALERT
+    const successAlert = document.getElementById("success-alert");
+    const errorAlert = document.getElementById("error-alert");
+
+    if (successAlert) {
+        successAlert.classList.add("show");
+        setTimeout(() => {
+            successAlert.classList.remove("show");
+        }, 3000);
+    }
+
+    if (errorAlert) {
+        errorAlert.classList.add("show");
+        setTimeout(() => {
+            errorAlert.classList.remove("show");
+        }, 3000);
+    }
+
     // SELECT
     const selects = document.querySelectorAll(".select");
 
