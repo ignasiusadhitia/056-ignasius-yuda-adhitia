cc60f2dc172a9c8562a1b1439a1ad709a740963f
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-05-31 01:38:22 +0700

    create welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,18 +5,15 @@
 @section('content')
 
     <section class="section-wrapper">
-        <h1>Think You Know West Java? Unleash Your Inner Jabar Guru with Jabar Trivia!</h1>
-        <p>Calling all West Java enthusiasts! Dive into the world of Jabar Trivia, the ultimate test of your Jabar
-            knowledge.
-            Answer captivating trivia questions with beautiful images, conquer the leaderboards, and emerge as a certified
-            Jabar
-            Guru! Create your own trivia challenges, rack up points, and battle your friends for trivia supremacy. It's time
-            to
-            unleash your inner Jabar expert and embark on a fun-filled learning adventure!</p>
+        <div class="hero">
+            <h1>Dive into <span>West Java</span> like never before!</h1>
+            <p>Jabar Trivia is the ultimate quiz app to test your knowledge on history, geography, culture, and more!
+            </p>
 
-        <a href="{{ route('register') }}" class="register-button-wrapper">
-            <button>Register</button>
-        </a>
+            <a href="{{ route('register') }}" class="register-button-wrapper">
+                <button>Register and Begin Exploring</button>
+            </a>
+        </div>
     </section>
 
 @endsection
